2af95239eb49b826123373a553aca4adb1850cd9
alpgiray <alpgirayacar@outlook.com>
2023-10-25 16:02:38 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+Console.WriteLine("Hello, World!");
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+foreach (var product in productManager.GetByUnitPrice(40,100))
+{
+
+    Console.WriteLine(product.ProductName);
+}
