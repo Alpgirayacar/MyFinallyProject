5f50c41db087ee2ed15b6a5682176bf587b85685
alpgiray <alpgirayacar@outlook.com>
2023-11-24 21:15:42 +0300

    WebAPI LAyer Added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,7 +10,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "ürün eklendi ";
         public static string ProductNameInvalid = "ürün ismi geçersiz ";
-        internal static string MaintenanceTime="Sistem bakımda";
-        internal static string ProductsListed="ürünler Listelendi ";
+        public static string MaintenanceTime="Sistem bakımda";
+        public static string ProductsListed="ürünler Listelendi ";
     }
 }
